ab8411e1959d1d8dc4ab3424b6b314158e2a8c36
CMF <clementmafa@gmail.com>
2025-08-09 22:50:25 +0200

    Add the App.js elements into the function : - a track array instead of spotify search, 3 states (searchResults, playlistName, playlistTracks), - start to setup addTrack and removeTrack, - add the props into Search results and Playlist

--- src/components/Jammming.js
+++ src/components/Jammming.js
@@ -1,9 +1,42 @@
+import React, {useState, useCallback} from 'react';
+import './componentsStyles/Jammming.css';
+
 import SearchBar from './SearchBar';
 import SearchResults from './SearchResult';
 import Playlist from './Playlist';
-import './componentsStyles/Jammming.css';
 
 function Jammming() {
+
+    const tracksArr = [
+      {name: "Baby", artist: "Royal Republic", album: "Weekend Man", id: "0001"},
+      {name: "Happy", artist: "C2C", album: "Tetra", id: "0002"},
+      {name: "Space Station No. 9", artist: "YOOOO", album: "NEXUS-2060", id: "0003"},
+      {name: "Space Station No. 6", artist: "CAPSULE", album: "NEXUS-2060", id: "0004"}
+  ];
+  
+  const [searchResults, setSearchResults] = useState(tracksArr);
+  const [playlistName, setPlaylistName] = useState("New Playlist");
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+
+  
+
+  const addTrack = useCallback(
+    (track) => {
+        if (playlistTracks.some((savedTrack) => savedTrack.id === track.id)){
+            return;
+        }
+        setPlaylistTracks((prevTracks) => [...prevTracks, track]);
+    },
+    [playlistTracks] 
+  );
+
+  const removeTrack = useCallback (
+    (track) => {
+        setPlaylistTracks((prevTracks) => prevTracks.filter((currentTrack) => currentTrack.id !== track.id));
+        }, []);
+    
+
+
   return (
         <section id="projects">
             <div class="breaking-news">
@@ -13,8 +46,8 @@ function Jammming() {
                 <p class="presentation">Search your favorites songs and made your playlist on Spotify (sorry for the users of Deezer...).</p>
                 <SearchBar />
                 <div class="results-playlist-container">
-                    <SearchResults />
-                    <Playlist />
+                    <SearchResults searchResults={searchResults} onAdd={addTrack}/>
+                    <Playlist playlistName={playlistName} playlistTracks={playlistTracks}/>
                 </div>
             </div>
         </section>
