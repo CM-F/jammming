96de425629b4d92eaeadf95be18a116c4c320f7e
CMF <clementmafa@gmail.com>
2025-08-10 11:22:06 +0200

    setup for <Playlist /> (must finish savePlaylist

--- src/components/Jammming.js
+++ src/components/Jammming.js
@@ -12,32 +12,34 @@ function Jammming() {
       {name: "Happy", artist: "C2C", album: "Tetra", id: "0002"},
       {name: "Space Station No. 9", artist: "YOOOO", album: "NEXUS-2060", id: "0003"},
       {name: "Space Station No. 6", artist: "CAPSULE", album: "NEXUS-2060", id: "0004"}
-  ];
+    ];
   
-  const [searchResults, setSearchResults] = useState(tracksArr);
-  const [playlistName, setPlaylistName] = useState("New Playlist");
-  const [playlistTracks, setPlaylistTracks] = useState([]);
+    const [searchResults, setSearchResults] = useState(tracksArr);
+    const [playlistName, setPlaylistName] = useState("New Playlist");
+    const [playlistTracks, setPlaylistTracks] = useState([]);
 
-  
+    const addTrack = useCallback(
+        (track) => {
+            if (playlistTracks.some((savedTrack) => savedTrack.id === track.id)){
+                return;
+            }
+            setPlaylistTracks((prevTracks) => [...prevTracks, track]);
+        },
+        [playlistTracks] 
+    );
 
-  const addTrack = useCallback(
-    (track) => {
-        if (playlistTracks.some((savedTrack) => savedTrack.id === track.id)){
-            return;
-        }
-        setPlaylistTracks((prevTracks) => [...prevTracks, track]);
-    },
-    [playlistTracks] 
-  );
-
-  const removeTrack = useCallback (
-    (track) => {
-        setPlaylistTracks((prevTracks) => prevTracks.filter((currentTrack) => currentTrack.id !== track.id));
-        }, []);
+    const removeTrack = useCallback (
+        (track) => {
+            setPlaylistTracks((prevTracks) => prevTracks.filter((currentTrack) => currentTrack.id !== track.id));
+            }, []);
     
+    const updatePlaylistName = useCallback((name) => {
+        setPlaylistName(name);
+    }, []);
 
+    let savePlaylist;
 
-  return (
+    return (
         <section id="projects">
             <div class="breaking-news">
                 <h2>JAMMMING</h2>
@@ -47,11 +49,11 @@ function Jammming() {
                 <SearchBar />
                 <div class="results-playlist-container">
                     <SearchResults searchResults={searchResults} onAdd={addTrack}/>
-                    <Playlist playlistName={playlistName} playlistTracks={playlistTracks}/>
+                    <Playlist playlistName={playlistName} playlistTracks={playlistTracks} onRemove={removeTrack} onSave={savePlaylist} />
                 </div>
             </div>
         </section>
-  );
+    );
 }
 
 export default Jammming;
